bebecc6bf9850833581f7534d0d9a23774fad41d
ANIRUDDH M BHAT <107295276+Maani02@users.noreply.github.com>
2023-09-13 12:33:09 +0530

    Update fifo_seq_list.sv

--- fifo_seq_list.sv
+++ fifo_seq_list.sv
@@ -2,8 +2,8 @@ class f_sequence_item extends uvm_sequence_item;
   rand bit i_wren;
   rand bit i_rden;
   rand bit [7:0] i_wdata;
-  bit full;
-  bit empty;
+  bit o_full;
+  bit o_empty;
   bit o_alm_full;
   bit o_alm_empty;
   bit [7:0] o_rdata;
@@ -12,8 +12,8 @@ class f_sequence_item extends uvm_sequence_item;
   `uvm_field_int(i_wren, UVM_ALL_ON)
   `uvm_field_int(i_rden, UVM_ALL_ON)
   `uvm_field_int(i_wdata, UVM_ALL_ON)
-  `uvm_field_int(full, UVM_ALL_ON)
-  `uvm_field_int(empty, UVM_ALL_ON)
+  `uvm_field_int(o_full, UVM_ALL_ON)
+  `uvm_field_int(o_empty, UVM_ALL_ON)
   `uvm_field_int(o_alm_full, UVM_ALL_ON)
   `uvm_field_int(o_alm_empty, UVM_ALL_ON)
   `uvm_field_int(o_rdata, UVM_ALL_ON)
